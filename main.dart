4e822bdb42f90ea7d77c82d09b31f2a0088d3451
João Cavalcante <51383938+JoaoCavalcante2005@users.noreply.github.com>
2022-04-08 23:46:12 -0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,24 +1,54 @@
-import 'dart:ui';
+import 'dart:html';
 
 import 'package:flutter/material.dart';
 
 main() {
-   runApp(App_flutter(titulo: 'texto'));
+  runApp(appwidget());
 }
 
-class App_flutter extends StatelessWidget{
-  final String titulo;
-
-  const App_flutter({Key? key, required this.titulo}) : super(key: key);
-
+class appwidget extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  int contador = 0;
+  @override
+  Widget build(BuildContext context) {
     return Container(
-     child: Center(child: Text(
-       titulo,
-       textDirection: TextDirection.ltr,
-       style: TextStyle(color: Colors.black, fontSize: 100.0)
-     )),
-   );
+      child: Center(
+          child: GestureDetector(
+        child: Text("top $contador"),
+        onTap: () {
+          setState(() {
+            contador++;
+          });
+        },
+      )),
+    );
+  }
+}
+
+class aplicativo_top extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Center(
+      child: Text("massa",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.black, fontSize: 80.0)),
+    ));
   }
 }
