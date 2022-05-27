2782d1f6cdad9f0787a57b16455ea5bfda998c70
João Cavalcante <51383938+JoaoCavalcante2005@users.noreply.github.com>
2022-05-26 22:41:26 -0300

    Add files via upload
    Código que aprendi no Alura

--- main.dart
+++ main.dart
@@ -1,20 +1,56 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
-import 'app_widget.dart';
 
-main() {
-  runApp(appwidget());
+void main() {
+  runApp(const MyApp());
 }
 
-class aplicativo_top extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Center(
-      child: Text("massa",
-          textDirection: TextDirection.ltr,
-          style: TextStyle(color: Colors.black, fontSize: 80.0)),
-    ));
+    return MaterialApp(
+      home: Scaffold(
+        body: ListaTransferencias(),
+        ),
+    );
   }
+}
+
+class ListaTransferencias extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(Transferencia(200,123456)),
+        ItemTransferencia(Transferencia(500,213745)),
+        ItemTransferencia(Transferencia(420,837583)),
+        ItemTransferencia(Transferencia(760,321732)),
+      ]
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget{
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context){
+    return Card(child: ListTile(
+      leading: Icon(Icons.monetization_on),
+      title: Text( _transferencia.valor.toString() ),
+      subtitle: Text( _transferencia.numeroConta.toString() ),
+    ),);
+  }
+}
+
+class Transferencia{
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor,this.numeroConta);
 }
